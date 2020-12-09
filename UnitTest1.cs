a7e8e494ec72a5b8dd24ea7a0f458b49560bdae3
IHergueta <59694418+IHergueta@users.noreply.github.com>
2020-12-09 11:43:56 +0100

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PruebasUnitarias;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            IPersona per = (IPersona)new Persona("Juan", "Palomares", 65);
+
+            string nombre = per.getNombre();
+
+            Assert.AreEqual("Juan", nombre);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            IPersona per = (IPersona)new Persona("Juan", "Palomares", 65);
+
+            string ape = per.getApellido();
+
+            Assert.AreEqual("Palomares", ape);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+
+            IPersona per = (IPersona)new Persona("Juan", "Palomares", 65);
+
+            int edad = per.getEdad();
+
+            Assert.AreEqual(65, edad);
+        }
+
+        
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+
+            IPersona per = (IPersona)new Persona("Juan", "Palomares", 65);
+
+            per.setNombre("Fernando");
+
+            Assert.AreEqual("Fernando",per.getNombre());
+        }
+    }
+}
